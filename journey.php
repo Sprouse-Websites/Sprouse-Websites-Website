a441724cd69df752cde727a98072a9ddc934c0f7
Joel Sprouse <joel.sprouse11@gmail.com>
2020-03-16 18:49:59 +0000

    Create journey.php

---
+++ journey.php
@@ -0,0 +1,47 @@
+<?php
+$page_title = "Our Journey | Sprouse Websites";
+$url = "journey.php";
+include 'includes/header.php';
+?>
+
+<h3>Our Journey</h3>
+<div class="timeline">
+  <div class="container left">
+    <div class="bubble">
+      <h2>2013</h2>
+      <p>A young boy is inspired by a school teacher to make websites. He takes it into his own hands to teach himself about websites. He starts creating pointless, rubbish websites, but it's a start.</p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="bubble">
+      <h2>2014</h2>
+      <p>"Websites are EPIC. Everyone needs a website. Why wouldn't anyone have a website???" At this stage way too many websites are created with no meaning or plan and many are abandoned part way through. </p>
+    </div>
+  </div>
+  <div class="container left">
+    <div class="bubble">
+      <h2>2018</h2>
+      <p>
+				<ol>
+					<li>HTML <i class="fa fa-check"></i></li>
+					<li>CSS <i class="fa fa-check"></i></li>
+					<li>JavaScript <i class="fa fa-check"></i> <sub>(Sort of)</sub></li>
+				</ol>
+				Next PHP
+			</p>
+    </div>
+  </div>
+	<div class="container right">
+    <div class="bubble">
+      <h2>2020</h2>
+      <p>This website is finally published and a few happy customers start appearing. </p>
+    </div>
+  </div>
+	<span style="background-color:red;color: white;top: 18pt;position: relative;padding: 5pt;border-radius: 10%;">
+		Watch this space
+	</span>
+</div>
+<br><br>
+<?php
+include 'includes/footer.php';
+?>
