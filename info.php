7648cc897de456cacbd045dfda69073f28efed74
Joel Sprouse <joel.sprouse11@gmail.com>
2020-03-16 18:50:03 +0000

    Create info.php

---
+++ info.php
@@ -0,0 +1,23 @@
+<?php
+$page_title = "Contact Us";
+$url = "info.php";
+include 'includes/header.php';
+include 'hidden/details.php';
+?>
+<!-- <h1>Are you a</h1>
+<a href="service.php" class="w3-bar-item w3-button" style="font-size:16pt;">Exsisting Customer</a>
+<vr style="font-size:18pt;"></vr>
+<a href="buy.php" class="w3-bar-item w3-button" style="font-size:16pt;">New Customer</a> -->
+
+
+<br>
+<h3>Sprouse Websites</h3>
+<br>
+<ul>
+	<li><a href="privacy.php" class="hover-u">To view our Privacy Policy Click Here</a>.</li>
+<li><a href="terms.php" class="hover-u">To view our Terms of Use Click Here</a>.</li>
+</ul>
+
+
+
+<?php include 'includes/footer.php'; ?>
