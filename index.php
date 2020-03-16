ac589feb59436b6965e07727bcd68018ae27b473
Joel Sprouse <joel.sprouse11@gmail.com>
2020-03-16 18:49:54 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+$page_title = "Sprouse Websites";
+$url = "";
+include 'includes/header.php';
+?>
+
+<style type="text/css">
+.main-text {
+	padding: 5pt;
+	text-align: center;
+}
+</style>
+
+<div class="main-text" style="font-size:16px;">
+	Sprouse Websites is a small company based in Croydon to make quality websites for anyone, particularly for small businesses in the surrounding area. We work with our clients to make sure that you are happy with their site, and will not stop till you are satisfied.
+</div>
+
+<div class="main-text">
+	<h2>Recent Posts</h2>
+	<table style="vertical-align:top;">
+		<tr>
+			<td style="width:30%">
+				<h3>NEW WEBSITE</h3>
+				<h6>5th Mar 2020</h6>
+				Look at this. We have got a new website! Woop woop! OK on a serious note, it's been a long time in the making but it's here. As time goes on, each project that is completed will be on this site. We're working on a load of projects at the moment s...
+				<br>
+				<a href="http://sprousewebsites.co.uk/blog/post/2020/03/new-website.php" class="read-more-link"><b>SEE FULL POST »</b></a>
+			</td>
+			<td style="width:30%">
+				<h3>NEW TERMS AND PRIVACY POLICY</h3>
+				<h6>10th Mar 2020</h6>
+				Our privacy Policy and Terms of Service are now online. You can see our Privacy Policy at http://www.sprousewebsites.co.uk/privacy.php and our Terms of Service at http://www.sprousewebsites.co.uk/terms.php. Please look at both of them. Thank...
+				<br>
+				<a href="http://sprousewebsites.co.uk/blog/post/2020/03/new-website.php" class="read-more-link"><b>SEE FULL POST »</b></a>
+			</td>
+			<!-- <td style="width:30%">
+			<h3>STARTING LOU'S CUSHIONS</h3>
+			<h6>13th Aug 2019</h6>
+			A new client has come to Sprouse Websites looking for a website to advertise their products. Up until this point, their products have only been sold to friends and family but now they have decided that it's time to promote their products. Watch this space as the ... <a class="read-more" href="posts/lou-cushions.html">READ MORE</a>
+		</td> -->
+	</tr>
+	<tr>
+		<td colspan="3">
+			<a href="blog" title="Go to our Blog" class="see-more-button">See more</a>
+		</td>
+	</tr>
+</table>
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
